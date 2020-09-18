f98cc0d49dc840f55dfa4c5c86384c706009a6d1
chen-qiong <chen-smile-qiong@163.com>
2020-09-18 21:58:28 +0800

    simplify the code

--- scheduleHtmlProvider.js
+++ scheduleHtmlProvider.js
@@ -1,24 +1,24 @@
-function scheduleHtmlProvider(iframeContent = "", frameContent = "", dom = document) {
-    //除函数名外都可编辑
-    //以下为示例，您可以完全重写或在此基础上更改
-                                
-const ifrs = dom.getElementsByTagName("iframe");
-const frs = dom.getElementsByTagName("frame");
-
-if (ifrs.length) {
-    for (let i = 0; i < ifrs.length; i++) {
-        const dom = ifrs[i].contentWindow.document;
-        iframeContent += scheduleHtmlProvider(iframeContent, frameContent, dom);
+function scheduleHtmlProvider(iframeContent = "", frameContent = "", dom = document) {                             
+    const ifrs = dom.getElementsByTagName("iframe");
+    const frs = dom.getElementsByTagName("frame");
+    /**
+     * 重用代码合并
+     * 增加浏览器兼容性
+     */
+    const htmlCollection = [...ifrs, ...frs];
+    if(!htmlCollection.length) {
+        return dom.querySelector('body').outerHTML
+    }
+    htmlCollection.forEach( (htmlSingle)=>{
+        let y = htmlSingle.contentWindow || htmlSingle.contentDocument
+        if(y === htmlSingle.contentWindow) {
+            dom = htmlSingle.contentWindow.document;
+            iframeContent += scheduleHtmlProvider(iframeContent, frameContent, dom);
+        }else {
+            dom = htmlSingle.contentDocument.body.parentElement;
+            frameContent += scheduleHtmlProvider(iframeContent, frameContent, dom);
         }
-} 
-if (frs.length) {
-    for (let i = 0; i < frs.length; i++) {
-        const dom = frs[i].contentDocument.body.parentElement;
-        frameContent += scheduleHtmlProvider(iframeContent, frameContent, dom);
-        }
-} 
-if(!ifrs.length && !frs.length){
-    return dom.querySelector('body').outerHTML
-}
-return dom.getElementsByTagName('html')[0].innerHTML + iframeContent+frameContent  
+    } )
+    
+    return dom.getElementsByTagName('html')[0].innerHTML + iframeContent + frameContent  
 }
